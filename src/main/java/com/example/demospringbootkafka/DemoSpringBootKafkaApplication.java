a27bc9a5c92edd6c8e3935aff6cccbf040349ecf
kubilaycaglayan <kblycaglayan@gmail.com>
2024-12-13 10:12:00 +0300

    Create producer

---
+++ src/main/java/com/example/demospringbootkafka/DemoSpringBootKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringbootkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringBootKafkaApplication.class, args);
+    }
+
+}
